ea36fd75925850f9a3b0968dd93e9ca98051d827
meriem <meriemismahlarbi@gmail.com>
2018-10-09 13:33:33 +0200

    store added

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import productListOne from './components/productListOne.vue';
+import productListTwo from './components/productListTwo.vue';
+
+export default {
+  name: 'app',
+  components:{
+    'product-list-one':productListOne,
+    'product-list-two':productListTwo,
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
